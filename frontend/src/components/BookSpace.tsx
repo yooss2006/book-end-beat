310c588562a437639b78b0439e8003dae14b693b
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-06-10 16:20:31 +0900

    책장 레이아웃 구성

---
+++ frontend/src/components/BookSpace.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BookSpace = () => {
+  return (
+    <ul className="BookSpace">
+      <li></li>
+    </ul>
+  );
+};
+
+export default BookSpace;
--- frontend/src/pages/BookShelf.tsx
+++ frontend/src/pages/BookShelf.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import BookSpace from "../components/BookSpace";
 
 const BookShelf = () => {
   return (
     <section className="BookShelf">
       <h2 className="blind">책장</h2>
+      <BookSpace />
     </section>
   );
 };

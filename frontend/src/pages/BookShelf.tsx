14aebec62dd4b8da9e13aaf86e2c96b56f0012f2
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-06-11 16:20:30 +0900

    카카오 book 검색 api 데이터 불러오기

--- frontend/src/pages/BookShelf.tsx
+++ frontend/src/pages/BookShelf.tsx
@@ -1,10 +1,29 @@
+import axios from "axios";
 import React from "react";
 import BookSpace from "../components/BookSpace";
 
 const BookShelf = () => {
+  console.log(process.env.REACT_APP_KAKAO_KEY);
+  axios({
+    method: "get", // 통신 방식
+    url: "https://dapi.kakao.com/v3/search/book?target=title&query=미움받을 용기",
+    headers: { Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_KEY}` },
+    params: { target: "title", query: "미움받을 용기" },
+    responseType: "json",
+  }).then(function (response) {
+    console.log(response);
+  });
+
   return (
     <section className="BookShelf">
       <h2 className="blind">책장</h2>
+      <nav>
+        <ul>
+          <li>전체</li>
+          <li>월 별</li>
+          <li>장르 별</li>
+        </ul>
+      </nav>
       <BookSpace />
     </section>
   );
